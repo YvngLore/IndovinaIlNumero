679a70db85c41d45faa25f718ed0f83c958e6807
YvngLore <socci.lorenzo@itismeucci.com>
2023-10-17 14:13:58 +0200

    Esercizio finito

--- Client/client/src/main/java/com/client/App.java
+++ Client/client/src/main/java/com/client/App.java
@@ -1,13 +1,43 @@
 package com.client;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Socket socket = new Socket("localhost", 3000);
+            System.out.println("Socket inizializzato");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            
+            Scanner scanner = new Scanner(System.in);
+            int risposta = 0;
+            
+            do{
+                String in = scanner.nextLine();
+                output.writeBytes(in + "\n");
+                
+                risposta = Integer.parseInt(input.readLine());
+                System.out.println(risposta);
+            }while(risposta != 3);
+
+            System.out.println(input.readLine());
+
+            scanner.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("ERRORE GENERICO");
+            System.out.println(e.getMessage());
+            return;
+        }
+
     }
 }
--- Server/server/src/main/java/com/server/App.java
+++ Server/server/src/main/java/com/server/App.java
@@ -1,13 +1,55 @@
 package com.server;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            System.out.println("Server inizializzato");
+
+            Socket socket = server.accept();
+            System.out.println("Un client si è connesso!");
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream()); 
+
+            Random random = new Random();
+            int numero_casuale = random.nextInt(1, 100);
+            int tentativi = 0;
+            int conversione = 0;
+            
+            do{
+                String numero_ricevuto = input.readLine();
+                conversione = Integer.parseInt(numero_ricevuto);
+
+                if(conversione < numero_casuale){
+                    output.writeBytes(1 + "\n");
+                    tentativi++;
+                }
+                if(conversione > numero_casuale){
+                    output.writeBytes(2 + "\n");
+                    tentativi++;
+                }
+            }while(conversione != numero_casuale);
+
+            tentativi++;
+            output.writeBytes(3 + "\n");
+            output.writeBytes(tentativi + "\n");
+
+            server.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("ERRORE GENERICO");
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 }
